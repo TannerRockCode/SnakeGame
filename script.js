6cf889c5c3ef282cb798a1457eebe32ed33fa999
Tanner <tannernthorne@gmail.com>
2024-08-18 16:20:39 -0600

    Create snake game interface
    Created console-like interface for desktop.
    JavaScript logic creates snake and allows
    snake to be moved across screen. Next will
    add start game by pressing 'enter' key
    which can also be used to restart the game.

--- script.js
+++ script.js
@@ -1,180 +1,94 @@
-// let directionY = 0;
-// let directionX = 1;
-// let snakeX = 0;
-// let snakeY = 0;
-
-//const beginGameButton = document.getElementById("beginGameBtn");
-//beginGameButton.addEventListener("click", beginGame);
-
 const gameMap = document.getElementById("game-map");
-console.log(gameMap);
 
-let snakePositions = [
-  { x: 20, y: 0 },
-  { x: 15, y: 0 },
-  { x: 10, y: 0 },
-  { x: 5, y: 0 },
-  { x: 0, y: 0 },
+let y = 0;
+let x = 1;
+
+var keyPress = "x";
+console.log("Creating Game");
+
+var gameMapHeight = gameMap.clientHeight;
+var gameMapWidth = gameMap.clientWidth;
+
+console.log(gameMapHeight);
+console.log(gameMapWidth);
+
+const ColumnCount = gameMapHeight / 10;
+const RowCount = gameMapWidth / 10;
+
+let snake = [
+  { x: 20, y: 20 },
+  { x: 19, y: 20 },
+  { x: 18, y: 20 },
+  { x: 17, y: 20 },
+  { x: 16, y: 20 },
 ];
 
-let snakeSegmentSize = 5;
+addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowLeft":
+      x = -1;
+      keyPress = "x";
+      console.log("ArrowLeft");
+      break;
+    case "ArrowRight":
+      x = 1;
+      keyPress = "x";
+      console.log("ArrowRight");
+      break;
+    case "ArrowUp":
+      y = -1;
+      keyPress = "y";
+      console.log("ArrowUp");
+      break;
+    case "ArrowDown":
+      y = 1;
+      keyPress = "y";
+      console.log("ArrowDown");
+      break;
+  }
+});
 
-function draw() {
-  console.log("running draw");
-  gameMap.innerHTML = "";
-  drawSnake();
-  // const testElement = document.createElement("div");
-  // testElement.className = "testElement";
-  //testElement.style.gridColumn = 100;
-  //testElement.style.gridRow = 100;
-  //gameMap.appendChild(testElement);
-}
-
-function updateFrame() {
-  console.log("Updating Frame");
-  let snakeHead = snakePositions[0];
-  let directionX = 1;
-  let directionY = 0;
-  snakeDirectionX = snakeHead.x;
-  snakeDirectionY = snakeHead.y;
-  snakeDirectionX += directionX * snakeSegmentSize;
-  snakeDirectionY += directionY * snakeSegmentSize;
-
-  snakePositions.unshift({ x: snakeDirectionX, y: snakeDirectionY });
-  snakePositions.pop();
-  draw();
+function clearGameMap() {
+  while (gameMap.firstChild) gameMap.removeChild(gameMap.firstChild);
 }
 
 function drawSnake() {
-  snakePositions.forEach((segment) => {
+  snake.forEach((segment) => {
     const snakeSegment = document.createElement("div");
-    snakeSegment.className = "snake";
-    snakeSegment.style.gridColumn = segment.x;
+    snakeSegment.setAttribute("class", "snake");
+    snakeSegment.style.width = 10;
+    snakeSegment.style.height = 10;
+    snakeSegment.style.backgroundColor = "white";
     snakeSegment.style.gridRow = segment.y;
+    snakeSegment.style.gridColumn = segment.x;
     gameMap.appendChild(snakeSegment);
   });
 }
 
-// function createElement(tag, className) {
-//   const element = document.createElement(tag);
-//   element.className = className;
-// }
+function UpdateGame() {
+  clearGameMap();
+  UpdateSnake();
+  drawSnake();
+}
 
-setInterval(updateFrame, 10000);
+function UpdateSnake() {
+  let snakeHead = snake[0];
+  let snakeHeadNext = snakeHead;
+  //if
+  switch (keyPress) {
+    case "x":
+      snakeHeadNext = { x: snakeHead.x + x, y: snakeHead.y };
+      break;
+    case "y":
+      snakeHeadNext = { x: snakeHead.x, y: snakeHead.y + y };
+      console.log("y value:");
+      console.log(y);
+      console.log("snakeYUpdate");
+      break;
+  }
+  console.log(snakeHeadNext);
+  snake.unshift(snakeHeadNext);
+  snake.pop();
+}
 
-// function drawSnake() {
-//   snakePositions.forEach((position) => {
-//     const snakeSection = createGameElement("div", "snake");
-//     setPosition(snakeSection, position);
-//     gameMap.appendChild(snakeSection);
-//   });
-// }
-
-// function createGameElement(tag, className) {
-//   const gameElement = document.createElement(tag);
-//   gameElement.className = className;
-// }
-
-// function setPosition(element, position) {
-//   element.style.gridColumn = position.x;
-//   element.style.gridRow = position.y;
-// }
-
-// draw();
-
-// function beginGame() {
-//   let gameOver = false;
-//   document.getElementById("beginGameBtn").innerHTML = "End Game";
-//   document.getElementById("gameMap").style.backgroundColor = "Yellow";
-
-//   squareSize = 5;
-//   rows = 50;
-//   columns = 50;
-
-//   let snake = document.createElement("div");
-//   snake.setAttribute("id", "Snake");
-//   snake.setAttribute("style", "width: 500px");
-//   snake.setAttribute("style", "height: 100px");
-//   snake.backgroundColor = "Yellow";
-
-//   let snakeDirectionX = 0;
-//   let snakeDirectionY = 0;
-
-//   gameMap.innerHTML = "";
-
-//   gameMap = document.getElementById("gameMap");
-//   gameMap.height = rows * squareSize;
-//   gameMap.width = columns * squareSize;
-
-//   const node = document.createElement("li");
-//   const textnode = document.createTextNode("Water");
-//   node.appendChild(textnode);
-//   document.body.appendChild(node);
-
-//   setInterval(update, 10000);
-
-//   document.addEventListener("keydown", (event) => {
-//     const keyPressed = event.code;
-//     if (event.code === "ArrowUp") {
-//       directionY = -1;
-//       directionX = 0;
-//     }
-//     if (keyPressed === "ArrowDown") {
-//       directionY = 1;
-//       directionX = 0;
-//     }
-//     if (keyPressed === "ArrowLeft") {
-//       directionY = 0;
-//       directionX = -1;
-//     }
-//     if (keyPressed === "ArrowRight") {
-//       directionY = 0;
-//       directionX = 1;
-//     }
-//   });
-
-//   function update() {
-//     if (gameOver == true) return;
-//     context.fillStyle = "Orange";
-//     context.fillRect(0, 0, gameMap.height, gameMap.width);
-
-//     context.fillStyle = "Purple";
-//     context.fillRect(199, 220, 5, 5);
-
-//     context.fillStyle = "Green";
-//     let snakeHead = snakePositions[0];
-//     snakeDirectionX = snakeHead.x;
-//     snakeDirectionY = snakeHead.y;
-//     snakeDirectionX += directionX * 5;
-//     snakeDirectionY += directionY * 5;
-
-//     snakePositions.unshift({ x: snakeDirectionX, y: snakeDirectionY });
-//     snakePositions.pop();
-
-//     snake.backgroundColor = "blue";
-
-//     // for (let i = 0; i < snakePositions.length; i++) {
-//     //   snake.style.gridColumn = snakePositions[i].x;
-//     //   snake.style.gridRow = snakePositions[i].y;
-//     // }
-
-//     document.getElementById("gameMap").appendChild(snake);
-
-//     // for (let i = 0; i < snake.length; i++) {
-//     //   context.fillRect(snake[i].x, snake[i].y, 5, 5);
-//     // }
-
-//     if (GameOverCheck(snake)) GameOver(context);
-//   }
-
-//   function GameOverCheck(snake) {
-//     if (snakePositions[0].x < 0 || snakePositions[0].x > 500) return true;
-//   }
-
-//   function GameOver(context) {
-//     gameOver = true;
-//     console.log("Game Over");
-//     context.fillStyle = "Black";
-//     context.fillRect(0, 0, gameMap.height, gameMap.width);
-//   }
-// }
+setInterval(UpdateGame, 100);
